b1d785547923c764fcfcef36a1714aa83d3f2ba9
Eligijus Krepsta <k.eligijus@gmail.com>
2019-07-15 17:02:53 +0300

    generate xml in prod only

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const { buildXml, writeFile } = require('./lib/main')
 
 module.exports = (options = {}, ctx) => ({
-  async ready() {
-    writeFile({
-      outDir: ctx.outDir,
-      filename: options.filename || 'podcast.xml',
-      xml: buildXml({feedOptions: options.feedOptions, pages: ctx.pages})
-    })
-  },
-
-  updated() {
+  generated() {
     writeFile({
       outDir: ctx.outDir,
       filename: options.filename || 'podcast.xml',
